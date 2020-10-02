837e1d038cb88d398b47e3f246ac53f4b7787c22
Valeria Koroleva <valeriia.koroleva@gmail.com>
2020-10-03 00:36:18 +0300

    feat: tests done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let min = Math.min.apply(null, array);
+        return min;
+    };
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let max = Math.max.apply(null, array);
+        return max;
+    };
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0){
+        return 0;
+      } else {
+        return array.reduce((acc, item) => (acc + item), 0) / array.length;
+      };
 }
+
